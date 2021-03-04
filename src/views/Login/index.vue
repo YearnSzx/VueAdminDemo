bc7ab1fc32daa41fbe92415ed8c4c1fde8e090f5
YearnSzx <yearnszx@qq.com>
2021-03-04 15:00:57 +0800

    修改后第一次上传

---
+++ src/views/Login/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="Login">
+    <div class="login-wrap">
+      <ul class="menu-tap">
+        <li class="current">登陆</li>
+        <li>注册</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#Login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tap {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background: rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+module.exports = {
+  // 基本路径
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  // 输出文件目录
+  outputDir: process.env.NODE_ENV === "production" ? "dist" : "devdist",
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: false,
+  /**
+   * webpack配置,see
+   * **/
+
+  chainWebpack: config => {},
+  configureWebpack: config => {},
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // css相关配置
+  css: {
+    // // 是否使用css分离插件 ExtractTextPlugin
+    // extract: true,
+    // // 开启 CSS source maps?
+    // sourceMap: false,
+    // // css预设器配置项
+    // loaderOptions:{
+    //   sass:{
+    //     data:'@import "./src/styles/main.scss";'
+    //   }
+    // },
+    // // 启用 CSS modules for all css / pre-processor files.
+    // modules: false
+  },
+  parallel: require("os").cpus().length > 1,
+  pwa: {},
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: true, // 编译完成是否打开网页
+    host: "0.0.0.0", // 指定使用地址，默认localhost,0.0.0.0代表可以呗外界访问
+    port: 8080, // 访问端口
+    https: false, // 编译失败时刷新页面
+    hot: true, // 开启热加载
+    hotOnly: false,
+    proxy: null, // 设置代理
+    overlay: {
+      // 全屏模式下是否显示脚本错误
+      warning: true,
+      errors: true
+    },
+    before: app => {}
+  },
+  pluginOptions: {}
+};
